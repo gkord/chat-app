1db0e50cdf136a7909a757c83611c010d4228da2
Gabe Kugelmass <gkord@me.com>
2020-05-20 08:59:39 -0400

    backend set up

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const auth = (req, res, next) => {
+  const token = req.header('x-auth-token');
+
+  //check for token
+  if (!token) {
+    res.status(401).json({ msg: 'No token, authorization denied.' });
+  }
+
+  try {
+    //verify token
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    //add user from payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: 'Token is not valid.' });
+  }
+};
+
+module.exports = auth;
--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,73 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const auth = require('../middleware/auth');
 
 const User = require('../models/User');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/', async (req, res) => {
+/**
+ * @route   GET api/users
+ * @desc    Get all users
+ * @access  Private
+ */
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) throw Error('No users exist');
+    res.json(users);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+/**
+ * @route   POST api/auth/login
+ * @desc    Login user
+ * @access  Public
+ */
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Simple validation
+  if (!email || !password) {
+    return res.status(400).json({ msg: 'Please enter all fields' });
+  }
+
+  try {
+    // Check for existing user
+    const user = await User.findOne({ email });
+    if (!user) throw Error('User Does not exist');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw Error('Invalid credentials');
+
+    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: 3600 });
+    if (!token) throw Error('Couldnt sign the token');
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
+
+/**
+ * @route   POST api/users
+ * @desc    Register new user
+ * @access  Public
+ */
+
+router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
 
   // Simple validation
@@ -48,6 +109,22 @@ router.post('/', async (req, res) => {
     });
   } catch (e) {
     res.status(400).json({ error: e.message });
+  }
+});
+
+/**
+ * @route   GET api/auth/user
+ * @desc    Get user data
+ * @access  Private
+ */
+
+router.get('/user', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    if (!user) throw Error('User Does not exist');
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
   }
 });
 
